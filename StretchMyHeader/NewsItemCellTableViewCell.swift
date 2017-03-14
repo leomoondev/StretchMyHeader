943650b2c17a2bb43d7c216ef4a9357fe9a9a9a9
Leo Moon <leomoondev@gmail.com>
2017-03-14 11:55:53 -0700

    Update UI on main table view controller

---
+++ StretchMyHeader/NewsItemCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsItemCellTableViewCell.swift
+//  StretchMyHeader
+//
+//  Created by Hyung Jip Moon on 2017-03-14.
+//  Copyright © 2017 leomoon. All rights reserved.
+//
+
+import UIKit
+
+class NewsItemCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
